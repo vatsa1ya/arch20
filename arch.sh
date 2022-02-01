115133d61ad14eb47889902d23125c48c0f87538
v n <vakchodtech@gmail.com>
2022-02-01 15:11:07 +0000

    Replace arch.sh

--- arch.sh
+++ arch.sh
@@ -19,17 +19,17 @@ nano /etc/hostname
 nano /etc/hosts
 
 echo 'Adding User And Setting Root Password'
-read -p 'Username: ' user
-echo 'Set Root Password'
-passwd
+read -p 'Set Username: ' user
 useradd -m $user
 echo 'Set User Password'
 passwd $user
-echo 'Creating User Groups'
+echo 'Set Root Password'
+passwd
+
 usermod -aG wheel,audio,video,storage,optical $user
 
 EDITOR=nano visudo
-echo 'Installing Grub'
+echo 'Installing GRUB'
 pacman -S grub efibootmgr dosfstools mtools os-prober -y
 
 mkdir /boot/EFI
@@ -42,8 +42,21 @@ grub-mkconfig -o /boot/grub/grub.cfg
 
 nano /etc/pacman.conf
 
-pacman -Sy plasma-meta steam discord fish neofetch htop telegram-desktop obs-studio code vlc -y
+pacman -Sy plasma-meta gparted steam ufw konsole dolphin dolphin-plugins packagekit-qt5 pulseaudio pulseaudio-bluetooth bluez bluez-utils discord fish neofetch htop telegram-desktop obs-studio code vlc -y
 
-exit
-umount -a
-reboot
+### Enabling Services ###
+
+echo '### Enabling Services ###'
+sudo systemctl enable NetworkManager
+sudo systemctl enable sddm
+sudo systemctl enable ufw
+sudo systemctl enable bluetooth
+
+### Setting Up Firewall ###
+
+echo '### Setting Up Firewall ###'
+ufw default deny
+ufw allow from 192.168.0.0/24
+ufw deny Deluge
+ufw deny ssh
+ufw enable
