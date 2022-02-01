24a607e112b83b5bf2163e172b191a8552e7bb7f
v n <vakchodtech@gmail.com>
2022-02-01 11:15:19 +0000

    Replace arch.sh

--- arch.sh
+++ arch.sh
@@ -18,15 +18,18 @@ locale-gen
 nano /etc/hostname
 nano /etc/hosts
 
+echo 'Adding User And Setting Root Password'
 read -p 'Username: ' user
+echo 'Set Root Password'
 passwd
 useradd -m $user
+echo 'Set User Password'
 passwd $user
-
+echo 'Creating User Groups'
 usermod -aG wheel,audio,video,storage,optical $user
 
 EDITOR=nano visudo
-
+echo 'Installing Grub'
 pacman -S grub efibootmgr dosfstools mtools os-prober -y
 
 mkdir /boot/EFI
@@ -36,7 +39,9 @@ mount /dev/nvme0n1p1 /boot/EFI
 grub-install --target=x86_64-efi --bootloader-id=GRUB --recheck
 
 grub-mkconfig -o /boot/grub/grub.cfg
+
 nano /etc/pacman.conf
+
 pacman -Sy plasma-meta steam discord fish neofetch htop telegram-desktop obs-studio code vlc -y
 
 exit
