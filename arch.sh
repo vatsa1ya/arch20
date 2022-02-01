84a4c06ae4c180a0d0d90f45448fcfb64f0a0bd1
v n <vakchodtech@gmail.com>
2022-02-01 10:26:01 +0000

    arch install

---
+++ arch.sh
@@ -0,0 +1,41 @@
+#! /bin/bash
+
+## SettingTime ##
+echo Setting Time
+ln -sf /usr/share/zoneinfo/Region/City /etc/localtime
+hwclock --systohc
+
+## Installing Essentials ##
+
+echo '###Installing Essentials###'
+pacman -S nano sudo networkmanager base-devel -y
+
+## Generating Locales ###
+
+nano /etc/locale.gen
+locale-gen
+
+nano /etc/hostname
+nano /etc/hosts
+
+read -p 'Username: ' user
+passwd
+useradd -m $user
+passwd $user
+
+usermod -aG wheel,audio,video,storage,optical $user
+
+EDITOR=nano visudo
+
+pacman -S grub efibootmgr dosfstools mtools os-prober
+
+mkdir /boot/EFI
+
+mount /dev/nvme0n1p1 /boot/EFI
+
+grub-install --target=x86_64-efi --bootloader-id=GRUB --recheck
+
+grub-mkconfig -o /boot/grub/grub.cfg
+nano /etc/pacman.conf
+pacman -Sy plasma-meta steam discord fish neofetch htop telegram-desktop obs-studio code vlc
+
