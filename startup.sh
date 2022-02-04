52ac644afc47ecdd7fe21ae8cbeaae7bdaa487a1
v n <vakchodtech@gmail.com>
2022-02-04 09:13:46 +0000

    Replace startup.sh

--- startup.sh
+++ startup.sh
@@ -1,13 +1,14 @@
-cd \installation/
+installation/
 ./ufw-setup.sh
 
 fish
 
-rm -rf ~/.config/fish/config.fish
-mkdir ~/.config/fish/functions
+sudo rm -rf ~/.config/fish/config.fish
+sudo mkdir ~/.config/fish/functions
 
-cp \installation\fish_prompt.fish ~/.config/fish/functions/fish_prompt.fish
-cp \installation\config.fish ~/.config/fish/config.fish
+cp fish_prompt.fish ~/.config/fish/functions/fish_prompt.fish
+cp config.fish ~/.config/fish/config.fish
 
+chsh -s /bin/fish
 exit
 
