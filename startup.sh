0028eedf4c9b20a7e2b5ea87d17fb868acd040df
v n <vakchodtech@gmail.com>
2022-02-04 08:30:58 +0000

    Upload New File

---
+++ startup.sh
@@ -0,0 +1,10 @@
+cd \installation/
+./ufw-setup.sh
+
+fish
+
+rm -rf ~/.config/fish/config.fish
+mkdir ~/.config/fish/functions
+
+cp \installation\fish_prompt.fish ~/.config/fish/functions/fish_prompt.fish
+cp \installation\config.fish ~/.config/fish/config.fish
