ebe11e37e16a64c32fcc71c9adfbe69dc99dbbaa
v n <vakchodtech@gmail.com>
2022-02-05 11:06:19 +0000

    Replace startup.sh

--- startup.sh
+++ startup.sh
@@ -1,7 +1,12 @@
 installation/
+sudo chmod +x ufw-setup.sh
+sudo chmod +x hp1020.sh
+
 ./ufw-setup.sh
 
 fish
+
+exit
 
 sudo rm -rf ~/.config/fish/config.fish
 sudo mkdir ~/.config/fish/functions
@@ -10,5 +15,5 @@ cp fish_prompt.fish ~/.config/fish/functions/fish_prompt.fish
 cp config.fish ~/.config/fish/config.fish
 
 chsh -s /bin/fish
-exit
 
+./hp1020.sh
